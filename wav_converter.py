d00a5c07bce7ac301eb9708669a62e7317521c10
Prashasst Dongre <prashassst@gmail.com>
2024-10-05 15:58:10 +0530

    update 2.1

--- wav_converter.py
+++ wav_converter.py
@@ -4,7 +4,7 @@ import subprocess
 def convert_m4a_to_wav(folder_path):
     # Get a list of all files in the specified folder
     for filename in os.listdir(folder_path):
-        if filename.endswith('.m4a'):
+        if filename.endswith('.opus'):
             # Define the full path for the input .m4a and output .wav files
             old_ext = os.path.join(folder_path, filename)
             wav_file = os.path.join(folder_path, f"{os.path.splitext(filename)[0]}.wav")
@@ -15,5 +15,5 @@ def convert_m4a_to_wav(folder_path):
 
 if __name__ == "__main__":
     # Specify the folder containing the .m4a files
-    folder_path = "C:\Users\prash\.cache\huggingface\datasets\downloads\extracted\46da4d94bea6a89203c499f996e08c56bfc317143ef9e4a28fee1fee4a595e40"  
+    folder_path = r"E:\New-Codes\Repositories\IdentifyMe-AI\VoiceData\Test"  
     convert_m4a_to_wav(folder_path)
